7c22e7185d49915cebdba64f3e2815e2d98aee5c
teguhnyamanblg88-glitch <teguhnyaman.blg88@gmail.com>
2026-01-10 04:09:46 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,38 @@
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    ContextTypes,
-    filters
-)
 from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 import os
 
-# TOKEN dari Render Environment Variable
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("TOKEN")  # nanti di Railway / Heroku
+OWNER_ID = int(os.getenv("OWNER_ID"))  # ID telegram kamu
 
-# ===== FUNGSI DEBUG FILE_ID =====
-async def get_file_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+# /start
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("✅ Bot hidup & siap menerima video")
+
+# terima video
+async def handle_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.effective_user.id != OWNER_ID:
+        await update.message.reply_text("⛔ Bot private")
+        return
+
+    # VIDEO
     if update.message.video:
-        await update.message.reply_text(update.message.video.file_id)
+        file_id = update.message.video.file_id
 
-# ===== BUILD APP =====
+    # DOCUMENT VIDEO
+    elif update.message.document and update.message.document.mime_type.startswith("video"):
+        file_id = update.message.document.file_id
+
+    else:
+        await update.message.reply_text("Bukan video")
+        return
+
+    # kirim ulang video
+    await update.message.reply_video(file_id)
+
 app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.VIDEO | filters.Document.VIDEO, handle_video))
 
-# ===== DAFTARKAN HANDLER =====
-app.add_handler(MessageHandler(filters.VIDEO, get_file_id))
-
-# ===== JALANKAN BOT =====
+print("Bot berjalan...")
 app.run_polling()
